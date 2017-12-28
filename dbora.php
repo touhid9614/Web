d22261571378498b0a74adf792b78ddd715e957b
Shahriar Rahman Zahin <xahin96@gmail.com>
2017-12-28 14:19:16 +0600

    xahin96
    data part update

--- dbora.php
+++ dbora.php
@@ -2,6 +2,8 @@
 // Create connection to Oracle
 
 function getJSONFromDB($sql){
+	
+	
 $conn = odbc_connect("127.0.0.1", "adbms", "pass");
 if (!$conn) {
    die('Erro');
@@ -17,17 +19,12 @@ if (!$conn) {
 // Close the Oracle connection
 odbc_close($conn);
 }	
-
-function insertIntoDB($sql){
+	
+	function insertIntoDB($sql){
 	$conn = odbc_connect('127.0.0.1','adbms','pass');
 	//echo $sql;
 	$result = odbc_exec($conn, $sql) or die(odbc_errormsg($conn));	
 	odbc_close($conn);
 }
-
-
-
-	
-	
 
 ?>
